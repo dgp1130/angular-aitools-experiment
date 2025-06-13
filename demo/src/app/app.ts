b8ca9a4eee1e98626b73004a0e6dad4efe777fb9
Doug Parker <dgp1130@users.noreply.github.com>
2025-06-13 11:23:50 -0700

    Adds a service to the demo app to test with.

--- demo/src/app/app.ts
+++ demo/src/app/app.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+class MyService {}
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  providers: [
+    {
+      provide: MyService,
+      useClass: MyService,
+    },
+  ],
 })
 export class App {
   protected title = 'angular-aitools-demo';
