6c5d3eab2b7c420fd05726b6aac1bda35bc9d699
Doug Parker <dgp1130@users.noreply.github.com>
2025-06-13 11:20:13 -0700

    Add basic UI.

--- extension/src/app/app.ts
+++ extension/src/app/app.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  protected readonly response = signal<string>('');
+
+  protected async submit(evt: SubmitEvent): Promise<void> {
+    evt.preventDefault();
+
+    const form = evt.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const prompt = formData.get('prompt')!;
+
+    const response = await sendPrompt(prompt.toString());
+    this.response.set(response);
+  }
+}
+
+async function sendPrompt(prompt: string): Promise<string> {
+  return prompt;
+}
